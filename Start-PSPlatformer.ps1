65a693b3f878ec48f9e5461e29e21e118527f1d0
Gregory F Martin <greg@gregfmartin.org>
2025-11-19 19:24:24 -0500

    Moved code here

---
+++ Start-PSPlatformer.ps1
@@ -0,0 +1,210 @@
+using namespace System
+using namespace System.Collections.Generic
+
+Set-StrictMode -Version Latest
+
+###############################################################################
+#
+# PLAT4MUR IN POWERSHELL
+# WRITTEN BY NOT GARY (GREGORY F MARTIN)
+#
+# THERE'S NO REASON FOR THIS TO EXIST. OH WELL.
+#
+# CONTROLS:
+#    LEFT/RIGHT ARROW KEYS: MOVE
+#    SPACEBAR: JUMP
+#    Q: QUIT
+#
+###############################################################################
+
+
+$ErrorActionPreference = "Stop"
+
+# CONSTANTS
+[Int]$Script:GRAVITY        = 1
+[Int]$Script:JUMP_STRENGTH  = -3
+[Int]$Script:MAX_FALL_SPEED = 2
+[Int]$Script:GAME_SPEED     = 30
+
+# # = WALL, SPACE = AIR, @ = THING, X = GOAL, ^ = BAD TERRAIN (NOT IMPLEMENTED)
+[String[]]$Script:LevelData = @(
+    "############################################################",
+    "#                                                          #",
+    "#                                                          #",
+    "#      @                                                   #",
+    "#    #####                                           X     #",
+    "#                                              #############",
+    "#           ####                               #           #",
+    "#                  #                           #           #",
+    "#                 ###             #  ###       #           #",
+    "#                                 #            #           #",
+    "#        ###             ###      #            #           #",
+    "#                                 #            #           #",
+    "#   #                             ##############           #",
+    "#   #                                                      #",
+    "############################################################"
+)
+
+[Int]$Script:MapHeight = $Script:LevelData.Count
+[Int]$Script:MapWidth  = $Script:LevelData[0].Length
+
+$Script:Player = [PSCustomObject]@{
+    X          = 2
+    Y          = 2
+    VX         = 0
+    VY         = 0
+    IsGrounded = $false
+    Symbol     = '@'
+    Color      = "Cyan"
+}
+
+For([Int]$R = 0; $R -LT $Script:MapHeight; $R++) {
+    If($Script:LevelData[$R].Contains('@')) {
+        $Script:Player.Y = $R
+        $Script:Player.X = $Script:LevelData[$R].IndexOf('@')
+
+        # REMOVE @ FROM MAP DATA SO WE DON'T COLLIDE WITH OUR SPAWN POINT
+        $Script:LevelData[$R] = $Script:LevelData[$R].Replace('@', ' ')
+    }
+}
+
+$Script:Running = $true
+$Script:Victory = $false
+
+Function Draw-Screen {    
+    [Console]::SetCursorPosition(0, 0)
+    
+    [List[String]]$Frame = [List[String]]::new()
+
+    For([Int]$Y = 0; $Y -LT $Script:MapHeight; $Y++) {
+        [Char[]]$Line = $Script:LevelData[$Y].ToCharArray()
+        
+        # DRAW PLAYER IF ON THIS LINE
+        If($Y -EQ $Script:Player.Y) {
+            If($Script:Player.X -GE 0 -AND $Script:Player.X -LT $Line.Length) {
+                $Line[$Script:Player.X] = $Script:Player.Symbol
+            }
+        }
+        
+        $Frame.Add([String]::new($Line))
+    }
+
+    $Frame.Add("POS: $($Script:Player.X),$($Script:Player.Y) | SPACE: Jump | Q: Quit")
+    
+    ForEach($L in $Frame) {
+        If($L -MATCH 'X') {
+            Write-Host $L -ForegroundColor Yellow -NoNewline
+        } ElseIf($L -MATCH '@') { 
+            Write-Host $L -ForegroundColor Cyan -NoNewline 
+        } Else {
+            Write-Host $L -ForegroundColor Gray -NoNewline
+        }
+        
+        Write-Host ''
+    }
+}
+
+Function Test-Collision {
+    Param(
+        [Int]$X,
+        [Int]$Y
+    )
+
+    If($Y -LT 0 -OR $Y -GE $Script:MapHeight -OR $X -LT 0 -OR $X -GE $Script:MapWidth) {
+        Return $true
+    }
+    
+    [Char]$C = $Script:LevelData[$Y][$X]
+    
+    If($C -EQ 'X') {
+        $Script:Victory = $true
+        $Script:Running = $false
+
+        Return $false
+    }
+
+    If($C -EQ '#') {
+        Return $true
+    }
+    
+    Return $false
+}
+
+Clear-Host
+
+Write-Host "`e[?25l"
+
+Try {
+    While($Script:Running) {
+        $Key = $null
+        If([Console]::KeyAvailable -EQ $true) {
+            $Key = [Console]::ReadKey($true).Key
+            While([Console]::KeyAvailable -EQ $true) {
+                $Dummy = [Console]::ReadKey($true)
+            }
+        }
+
+        If($Key -EQ "Q") {
+            $Script:Running = $false
+        }
+
+        # HORIZONTAL MOVEMENT
+        If($Key -EQ "LeftArrow") {
+            $Script:Player.VX = -1
+        } ElseIf($Key -EQ "RightArrow") {
+            $Script:Player.VX = 1
+        } Else {
+            $Script:Player.VX = 0
+        }
+
+        # APPLY HORIZONTAL VELOCITY
+        [Int]$NextX = $Script:Player.X + $Script:Player.VX
+        If(-NOT (Test-Collision $NextX $Script:Player.Y)) {
+            $Script:Player.X = $NextX
+        }
+
+        # VERTICAL MOVEMENT (GRAVITY)
+        $Script:Player.VY += $Script:GRAVITY
+        
+        # CAP FALLING SPEED
+        If($Script:Player.VY -GT $Script:MAX_FALL_SPEED) {
+            $Script:Player.VY = $Script:MAX_FALL_SPEED
+        }
+
+        # JUMP HANDLER
+        If($Key -EQ "Spacebar" -AND $Script:Player.IsGrounded) {
+            $Script:Player.VY         = $Script:JUMP_STRENGTH
+            $Script:Player.IsGrounded = $false
+        }
+
+        # APPLY VERTICAL VELOCITY
+        [Int]$NextY = $Script:Player.Y + $Script:Player.VY
+        
+        # VERTICAL COLLISION DETECTION ATTEMPT...
+        If(Test-Collision $Script:Player.X $NextY) {
+            # MOVING DOWN...
+            If($Script:Player.VY -GT 0) {
+                $Script:Player.IsGrounded = $true
+            }
+
+            # MOVING UP...
+            $Script:Player.VY = 0 
+        } Else {
+            $Script:Player.Y          = $NextY
+            $Script:Player.IsGrounded = $false
+        }
+
+        Draw-Screen
+
+        Start-Sleep -Milliseconds $Script:GAME_SPEED
+    }
+
+    [Console]::SetCursorPosition(0, $Script:MapHeight + 2)
+    If($Script:Victory) {
+        Write-Host 'GOT ''EM!' -ForegroundColor Green
+    } Else {
+        Write-Host 'YOU SUCK!' -ForegroundColor Yellow
+    }
+} Finally {
+    [Console]::CursorVisible = $true
+}
